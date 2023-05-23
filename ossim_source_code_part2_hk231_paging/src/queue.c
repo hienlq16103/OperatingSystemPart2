944a6531c38e4e0b2c64ca6cd331e46bc7eba29f
hieule0811 <hieu.le0811@hcmut.edu.vn>
2023-05-23 21:39:55 +0700

    Modified enqueue dequeue

--- ossim_source_code_part2_hk231_paging/src/queue.c
+++ ossim_source_code_part2_hk231_paging/src/queue.c
@@ -9,29 +9,20 @@ int empty(struct queue_t * q) {
 
 void enqueue(struct queue_t * q, struct pcb_t * proc) {
         /* TODO: put a new process to queue [q] */
+	if (q->size == MAX_QUEUE_SIZE) return;
 	q->proc[q->size] = proc;
-	q->size ++;	
+	q->size++;
+
 }
 
 struct pcb_t * dequeue(struct queue_t * q) {
-        /* TODO: return a pcb whose prioprity is the highest
-         * in the queue [q] and remember to remove it from q
-         * */
-	int max_prio = q->proc[0]->priority;
-    int max_slot = 0;
-    for (int i = 1; i < q->size; i++) {
-        if (q->proc[i]->priority > max_prio) {
-            max_prio = q->proc[i]->priority;
-            max_slot = i;
-        }
-    }
-    struct pcb_t *proc = q->proc[max_slot];
-    for (int i = max_slot; i < q->size - 1; i++) {
-        q->proc[i] = q->proc[i + 1];
-    }
-    q->size--;
-    return proc;
-	
-	return NULL;
+	if (empty(q)) return NULL;
+	struct pcb_t * proc = q->proc[0];
+	for (int i = 0; i < q->size - 1; i++) {
+		q->proc[i] = q->proc[i + 1];
+	}
+	q->size--;
+	q->slot--;
+	return proc;
 }
 
